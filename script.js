4df7b39438bb02f849e04f097d1f13a80d166cd4
emmosley <55521788+emmosley@users.noreply.github.com>
2019-09-22 22:35:36 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+//declaring the variables
+var eventType = prompt("What type of event are you going to?");
+var tempFahr = prompt("What is the weather?");
+
+//determing the type of event
+if (eventType === 'casual') {
+	result1 = "something comfy";
+  }
+    else if (eventType === 'semi-formal') {
+	result1 = "a polo";
+}
+   else if (eventType === 'formal') {
+	result1 = "a suit";
+}
+
+//determining the weather
+if (tempFahr < 54) { 
+	result2 = "a coat";
+  }
+    else if (54 <= tempFahr <= 70) {
+	result2 = "a jacket";
+}
+   else if (tempFahr > 70) {
+	result2 = "no jacket";
+}
+
+//final output
+var response = "Since it is " + tempFahr + "degrees and you are going to a " eventType ", you should wear " result1 + result2";
+
+
+console.log (response); 
